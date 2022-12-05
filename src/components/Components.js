50e9e490a66e1afa7f79d2d6e2975ec66b55225f
Regloom <egor.tokarev@outlook.com>
2022-12-05 16:03:05 +0100

    Add Toolbar Block and Mark buttons + handlers

--- src/components/Components.js
+++ src/components/Components.js
@@ -3,9 +3,14 @@ import {
   useSlateStatic,
   useSelected,
   useFocused,
-  ReactEditor
+  ReactEditor,
+  useSlate
 } from 'slate-react'
-import { Transforms} from 'slate'
+import {
+  Editor, 
+  Transforms, 
+  Element as SlateElement
+} from 'slate'
 import imageExtensions from 'image-extensions'
 import isUrl from 'is-url'
 import { cx, css } from '@emotion/css'
@@ -102,6 +107,111 @@ const InsertImageButton = () => {
   )
 }
 
+// Mark Buttons
+const isMarkActive = (editor, format) => {
+  const marks = Editor.marks(editor)
+  return marks ? marks[format] === true : false
+}
+
+const toggleMark = (editor, format) => {
+  const isActive = isMarkActive(editor, format)
+
+  if (isActive) {
+    Editor.removeMark(editor, format)
+  } else {
+    Editor.addMark(editor, format, true)
+  }
+}
+
+const MarkButton = ({ format, icon }) => {
+  const editor = useSlate();
+  return (
+    <Button
+      active={isMarkActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleMark(editor, format)
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  )
+}
+
+// Block buttons (h1-h3)
+const LIST_TYPES = ['numbered-list', 'bulleted-list']
+const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify']
+
+const isBlockActive = (editor, format, blockType = 'type') => {
+  const { selection } = editor
+  if (!selection) return false
+
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: n =>
+        !Editor.isEditor(n) &&
+        SlateElement.isElement(n) &&
+        n[blockType] === format,
+    })
+  )
+
+  return !!match
+}
+
+const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(
+    editor,
+    format,
+    TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
+  )
+  const isList = LIST_TYPES.includes(format)
+
+  Transforms.unwrapNodes(editor, {
+    match: n =>
+      !Editor.isEditor(n) &&
+      SlateElement.isElement(n) &&
+      LIST_TYPES.includes(n.type) &&
+      !TEXT_ALIGN_TYPES.includes(format),
+    split: true,
+  })
+  let newProperties;
+  if (TEXT_ALIGN_TYPES.includes(format)) {
+    newProperties = {
+      align: isActive ? undefined : format,
+    }
+  } else {
+    newProperties = {
+      type: isActive ? 'paragraph' : isList ? 'list-item' : format,
+    }
+  }
+  Transforms.setNodes(editor, newProperties)
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] }
+    Transforms.wrapNodes(editor, block)
+  }
+}
+const BlockButton = ({ format, icon }) => {
+  const editor = useSlate()
+  return (
+    <Button
+      active={isBlockActive(
+        editor,
+        format,
+        TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
+      )}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleBlock(editor, format)
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  )
+}
+
+// Toolbar 
 export const Toolbar = React.forwardRef( ({ className, ...props }, ref) => (
       <Menu
         {...props}
@@ -116,6 +226,15 @@ export const Toolbar = React.forwardRef( ({ className, ...props }, ref) => (
             margin-bottom: 20px;
           `
         )}> 
+        {/* Buttons for character styles */}
+        <MarkButton format="bold" icon="format_bold" />
+        <MarkButton format="italic" icon="format_italic" />
+        <MarkButton format="underline" icon="format_underlined" />
+        <MarkButton format="code" icon="code" />
+        <BlockButton format="h1" icon="looks_one" />
+        <BlockButton format="h2" icon="looks_two" />
+        <BlockButton format="h3" icon="looks_3" />
+        {/* Image button*/}
         <InsertImageButton />
       </Menu>
 ))
@@ -123,6 +242,26 @@ export const Toolbar = React.forwardRef( ({ className, ...props }, ref) => (
 export const Paragraph = ({ attributes, children }) => (
   <p {...attributes}>{children}</p>
 )
+
+export const StyledText = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong {...attributes}>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code {...attributes}>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em {...attributes}>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u {...attributes}>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
+}
 
 export const Image = ({ attributes, element, children }) => {
   const editor = useSlateStatic();
@@ -195,3 +334,4 @@ export const withImages = editor => {
 
   return editor
 }
+
