fa985dfb0ad62b0927802a55ba07b79208d81c8d
Regloom <egor.tokarev@outlook.com>
2022-12-05 13:03:58 +0100

    small refactoring move elements to Components.js

--- src/components/Components.js
+++ src/components/Components.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  useSlateStatic,
+  useSelected,
+  useFocused,
+  ReactEditor
+} from 'slate-react'
+import { Transforms} from 'slate'
+import imageExtensions from 'image-extensions'
+import isUrl from 'is-url'
 import { cx, css } from '@emotion/css'
 
 // export const Button = ({ className, active, ...props }) => {
@@ -10,7 +19,7 @@ import { cx, css } from '@emotion/css'
 //     return <span {...props} className={className} style={style} />;
 // };
 
-export const Button = React.forwardRef( ({ className, active, reversed, ...props}, ref) => (      
+const Button = React.forwardRef( ({ className, active, reversed, ...props}, ref) => (      
     <span {...props} ref={ref}
         className={cx(
           className,
@@ -28,7 +37,7 @@ export const Button = React.forwardRef( ({ className, active, reversed, ...props
     />
 ))
 
-export const Icon = React.forwardRef( ({ className, ...props }, ref) => (
+const Icon = React.forwardRef( ({ className, ...props }, ref) => (
       <span
         {...props}
         ref={ref}
@@ -43,7 +52,7 @@ export const Icon = React.forwardRef( ({ className, ...props }, ref) => (
       />
 ))
 
-export const Menu = React.forwardRef( ({ className, ...props }, ref) => (
+const Menu = React.forwardRef( ({ className, ...props }, ref) => (
       <div
         {...props}
         ref={ref}
@@ -60,6 +69,38 @@ export const Menu = React.forwardRef( ({ className, ...props }, ref) => (
         )}
       />
 ))
+// isImageUrl & insertImage funcs used by InsertImageButton & WithImages
+const isImageUrl = url => {
+  if (!url) return false
+  if (!isUrl(url)) return false
+  const ext = new URL(url).pathname.split('.').pop()
+  return imageExtensions.includes(ext)
+}
+
+const insertImage = (editor, url) => {
+  const text = { text: '' }
+  const image= { type: 'image', url, children: [text] }
+  Transforms.insertNodes(editor, image)
+}
+
+const InsertImageButton = () => {
+  const editor = useSlateStatic()
+  return (
+    <Button
+      onMouseDown={event => {
+        event.preventDefault()
+        const url = window.prompt('Enter the URL of the image:')
+        if (url && !isImageUrl(url)) {
+          alert('URL is not an image')
+          return
+        }
+        url && insertImage(editor, url)
+      }}
+    >
+      <Icon>image</Icon>
+    </Button>
+  )
+}
 
 export const Toolbar = React.forwardRef( ({ className, ...props }, ref) => (
       <Menu
@@ -74,6 +115,83 @@ export const Toolbar = React.forwardRef( ({ className, ...props }, ref) => (
             border-bottom: 2px solid #eee;
             margin-bottom: 20px;
           `
-        )}
-      />
+        )}> 
+        <InsertImageButton />
+      </Menu>
 ))
+
+export const Paragraph = ({ attributes, children }) => (
+  <p {...attributes}>{children}</p>
+)
+
+export const Image = ({ attributes, element, children }) => {
+  const editor = useSlateStatic();
+  const path = ReactEditor.findPath(editor, element);
+
+  const selected = useSelected();
+  const focused = useFocused();
+
+  return (
+    <div {...attributes}>
+      {children}
+      <div contentEditable={false} className={css`position: relative;`}>
+        <img  src={element.url} 
+              alt={element.alt} 
+              className={css`
+                display: block;
+                max-width: 100%;
+                max-height: 20em;
+                box-shadow: ${selected && focused ? '0 0 0 3px #B4D5FF' : 'none'};
+              `}
+        />
+        <Button
+          active
+          onClick={() => Transforms.removeNodes(editor, { at: path })}
+          className={css`
+            display: ${selected && focused ? 'inline' : 'none'};
+            position: absolute;
+            top: 0.5em;
+            left: 0.5em;
+            background-color: white;
+          `}
+        >
+          <Icon>delete</Icon>
+        </Button>
+      </div>
+    </div>
+  )}
+
+export const withImages = editor => {
+  const { insertData, isVoid } = editor
+
+  editor.isVoid = element => {
+    return element.type === 'image' ? true : isVoid(element)
+  }
+
+  editor.insertData = data => {
+    const text = data.getData('text/plain')
+    const { files } = data
+
+    if (files && files.length > 0) {
+      for (const file of files) {
+        const reader = new FileReader()
+        const [mime] = file.type.split('/')
+
+        if (mime === 'image') {
+          reader.addEventListener('load', () => {
+            const url = reader.result
+            insertImage(editor, url)
+          })
+
+          reader.readAsDataURL(file)
+        }
+      }
+    } else if (isImageUrl(text)) {
+      insertImage(editor, text)
+    } else {
+      insertData(data)
+    }
+  }
+
+  return editor
+}
--- src/components/SyncingEditor.js
+++ src/components/SyncingEditor.js
@@ -1,24 +1,13 @@
 // Import React dependencies.
 import React, { useState, useRef, useEffect } from 'react';
 // Import the Slate components and React plugin.
-import { Transforms, createEditor} from 'slate'
-import {
-  Slate,
-  Editable,
-  useSlateStatic,
-  useSelected,
-  useFocused,
-  withReact,
-  ReactEditor,
-} from 'slate-react'
+import { createEditor} from 'slate'
+import { Slate, Editable, withReact} from 'slate-react'
 import { withHistory } from 'slate-history'
-//other dependencies
-import imageExtensions from 'image-extensions'
-import isUrl from 'is-url'
-import { css } from '@emotion/css'
+
 //local dependencies
 import initialValue from '../util/initialValue';
-import { Button, Icon, Toolbar } from './Components';
+import { Toolbar, Paragraph, Image, withImages } from './Components';
 // request birdirectional connection between server and client
 import { io } from 'socket.io-client';
 
@@ -47,14 +36,13 @@ export const SyncingEditor = ({ groupId }) => {
   const [value, setValue] = useState(initialValue); //just for triggering re-rendering of Editor
 
   useEffect(() => {
-    
-    const getServerValue = async () => {
+    const getServerValue = async (groupId) => { //done for fancy ASYNC syntax!
+      console.log('here we go');
       const response = await fetch(`${ENDPOINT}/api/groups/${groupId}`);
-      const newValue = await response.json(); 
+      const newValue = await response.json();
       editor.children = newValue; //a new way to set editor content 
-      setValue(newValue);//just to trigger re-rendering! 
+      setValue(newValue);//just to trigger re-rendering!
     } 
-
     getServerValue(); //editor content from server
 
     const eventName = `new-remote-operations-${groupId}`;
@@ -80,6 +68,7 @@ export const SyncingEditor = ({ groupId }) => {
     const isAstChange = editor.operations.some(
       op => 'set_selection' !== op.type
     )
+
     if (isAstChange) { //content changed!
       setValue(value);
       //emit event to server
@@ -88,53 +77,13 @@ export const SyncingEditor = ({ groupId }) => {
           changedEditorId: id.current,
           newValue: value,
           groupId: groupId
-        },(response) => {
+        }
+        ,(response) => {
           //console.log(response); // "got it"
         });
       }
     } 
   }
-
-  const Paragraph = ({ attributes, children }) => (
-    <p {...attributes}>{children}</p>
-  )
-  
-  const Image = ({ attributes, element, children }) => {
-    const editor = useSlateStatic();
-    const path = ReactEditor.findPath(editor, element);
-  
-    const selected = useSelected();
-    const focused = useFocused();
-
-    return (
-      <div {...attributes}>
-        {children}
-        <div contentEditable={false} className={css`position: relative;`}>
-          <img  src={element.url} 
-                alt={element.alt} 
-                className={css`
-                  display: block;
-                  max-width: 100%;
-                  max-height: 20em;
-                  box-shadow: ${selected && focused ? '0 0 0 3px #B4D5FF' : 'none'};
-                `}
-          />
-          <Button
-            active
-            onClick={() => Transforms.removeNodes(editor, { at: path })}
-            className={css`
-              display: ${selected && focused ? 'inline' : 'none'};
-              position: absolute;
-              top: 0.5em;
-              left: 0.5em;
-              background-color: white;
-            `}
-          >
-            <Icon>delete</Icon>
-          </Button>
-        </div>
-      </div>
-    )}
   
   const renderElement = (props) => {
     switch (props.element.type) {
@@ -158,79 +107,9 @@ export const SyncingEditor = ({ groupId }) => {
   return (
     <>
       <Slate editor={editor} value={value} onChange={onChange} >
-        <Toolbar>
-          <InsertImageButton />
-        </Toolbar>
+        <Toolbar />
         <Editable renderElement={renderElement} placeholder="Enter some text"/>
       </Slate>
     </>
   );
 };
-
-//below are only images functions
-const withImages = editor => {
-  const { insertData, isVoid } = editor
-
-  editor.isVoid = element => {
-    return element.type === 'image' ? true : isVoid(element)
-  }
-
-  editor.insertData = data => {
-    const text = data.getData('text/plain')
-    const { files } = data
-
-    if (files && files.length > 0) {
-      for (const file of files) {
-        const reader = new FileReader()
-        const [mime] = file.type.split('/')
-
-        if (mime === 'image') {
-          reader.addEventListener('load', () => {
-            const url = reader.result
-            insertImage(editor, url)
-          })
-
-          reader.readAsDataURL(file)
-        }
-      }
-    } else if (isImageUrl(text)) {
-      insertImage(editor, text)
-    } else {
-      insertData(data)
-    }
-  }
-
-  return editor
-}
-
-const insertImage = (editor, url) => {
-  const text = { text: '' }
-  const image= { type: 'image', url, children: [text] }
-  Transforms.insertNodes(editor, image)
-}
-
-const isImageUrl = url => {
-  if (!url) return false
-  if (!isUrl(url)) return false
-  const ext = new URL(url).pathname.split('.').pop()
-  return imageExtensions.includes(ext)
-}
-
-const InsertImageButton = () => {
-  const editor = useSlateStatic()
-  return (
-    <Button
-      onMouseDown={event => {
-        event.preventDefault()
-        const url = window.prompt('Enter the URL of the image:')
-        if (url && !isImageUrl(url)) {
-          alert('URL is not an image')
-          return
-        }
-        url && insertImage(editor, url)
-      }}
-    >
-      <Icon>image</Icon>
-    </Button>
-  )
-}
